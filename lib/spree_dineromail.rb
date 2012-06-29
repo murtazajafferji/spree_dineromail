cf16493b958fc7bcc02851170ecfb7f9ad4cd4fb
Murtaza Jafferji <murtazajafferji@gmail.com>
2012-06-29 06:34:01 -0500

    checking

--- lib/spree_dineromail.rb
+++ lib/spree_dineromail.rb
@@ -10,6 +10,10 @@ module SpreeDineromail
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+      
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
     end
     
     initializer "spree_payment_network.register.payment_methods" do |app|
